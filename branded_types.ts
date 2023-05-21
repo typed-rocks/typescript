43b8b6ceea23eff4123dd47da04461f79df0881e
Christian Woerz <chwoerz@users.noreply.github.com>
2023-05-21 22:40:56 +0200

    Update branded_types.ts

--- branded_types.ts
+++ branded_types.ts
@@ -1,21 +1,25 @@
-function branded<T, Brand extends T & {__brand: symbol}>(checkFn: (input: T) => boolean): [(input: T) => input is Brand, Brand] {
-    const isBrand = function (input: T): input is Brand {
+function branded<PrimitiveType, BrandName, Brand extends PrimitiveType & {__brand: BrandName}>(
+    checkFn: (input: PrimitiveType) => boolean, brandName: BrandName
+): [(input: PrimitiveType) => input is Brand, Brand] {
+    const isBrand = function (input: PrimitiveType): input is Brand {
         return checkFn(input);
     }
 
     return [isBrand, {} as Brand];
 }
 
-const [isEmail, EmailObject] = branded((input: string) => true);
+const [isEmail, EmailObject] = branded((input: string) => true, 'ValidEmail' as const);
 
-type EmailAddress = typeof EmailObject;
-//   ^? 
+type ValidEmail = typeof EmailObject;
 
-function saveEmail(input: EmailAddress) {
+function sendEmail(input: ValidEmail) {
 
 }
 
-const a = 'adsf';
-if(isEmail(a)) {
-    saveEmail(a);
+const email = 'mail';
+if(isEmail(email)) {
+    const validEmail: ValidEmail = email;
+    sendEmail(email);
 }
+
+const invalidEmail: ValidEmail = email;
