0b1f081188663e6c4cf6ac9073d98a602fdeb7ad
Christian Woerz <chwoerz@users.noreply.github.com>
2024-07-06 15:44:59 +0200

    Update template_literals.ts

--- template_literals.ts
+++ template_literals.ts
@@ -5,8 +5,35 @@ type BoardPositions = `${ChessLetters}${ChessNumbers}`[];
 
 const b: BoardPositions = ['A1'];
 
-type RgbCssType = `rgb(${number},${number},${number})`;
 
-const rgbWrong: RgbCssType = 'asdf';
-const rgbCorrect: RgbCssType = 'rgb(1,1,1)';
+type RgbCss = `rgb(${number},${number},${number})`;
+type RgbaCss = `rgb(${number},${number},${number}${`,${number}` | ''})`;
 
+const rgbCss: RgbCss = 'rgb(1,2,3)';
+const rgbaCss: RgbaCss = 'rgb(1,2,3,2)';
+
+
+type GapType = 'margin' | 'padding';
+type GapPosition = 'left' | 'right' | 'top' | 'bottom';
+type GapCss = GapType | `${GapType}-${GapPosition}`;
+//   ^?
+
+type SizeType = 'rem' | 'px';
+type SizeCss = `${number}${SizeType}`;
+
+type MarginPadding = {
+  [Key in GapCss]?: SizeCss
+}
+
+const margin: MarginPadding = {
+  "margin-left": '1rem'
+}
+
+type FirstChar<T extends string> =  T extends `${infer head}${infer tail}` ? head : '';
+type StartsWithNumber<T extends string> = T extends `${infer head extends number}${infer tail}` ? head : '';
+
+type First = FirstChar<'hello youtube'>;
+//   ^?
+
+type Starts = StartsWithNumber<'1asdf'>;
+//   ^?
