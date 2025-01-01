5b42a70781a9c8818951cdcbdc099e5c59c661fa
Christian Woerz <4927839+chwoerz@users.noreply.github.com>
2025-01-01 17:31:10 +0100

    Update length.ts

--- length.ts
+++ length.ts
@@ -1,21 +1,73 @@
 
-type SimpleLength<T extends string, Counter extends 0[] = []> =
-  T extends `${string}${infer tail}`
-    ? SimpleLength<tail,  [...Counter, 0]>
+type Length<T extends string, Counter extends number[] = []> = 
+  T extends `${string}${infer Tail}`
+    ? Length<Tail, [...Counter, 0]>
     : Counter['length'];
 
-type IsTemplate<T extends string> = SimpleLength<T> extends 0
-  ? T extends ''
-    ? false
-    : true
-  : false;
+// Compare<1,2>
+type Compare<First extends number, Second extends number, Counter extends number[] = []> = 
+  First extends Second 
+  ? 'equal'
+  : Counter['length'] extends First
+    ? 'less'
+    : Counter['length'] extends Second
+      ? 'greater'
+      : Compare<First, Second, [...Counter, 0]>;
 
-type ContainsTemplate<T extends string> = T extends `${infer head}${infer tail}`
-  ? IsTemplate<head> extends true ? true : ContainsTemplate<tail> : IsTemplate<T>;
-type Hello = 'hello';
-type HelloTest = Length<`Hello ${Hello}Youtube`>;
-//   ^?
+type MaxLength<T extends string, Max extends number> =
+  Compare<Length<T>, Max> extends 'less' | 'equal' ? T : never;
 
-type Length<T extends string> = ContainsTemplate<T> extends true
-  ? never
-  : SimpleLength<T>;
+type MinLength<T extends string, Min extends number> =
+  Compare<Min, Length<T>> extends 'less' | 'equal' ? T : never;
+
+type InRange<T extends string, Min extends number, Max extends number> = 
+  MinLength<T, Min> & MaxLength<T, Max>;
+
+function maxOrThrow<T extends string, Max extends number>(
+  str: MaxLength<T, Max>, max: Max
+): string {
+  if(str.length > max) {
+    throw new Error(`${str} is longer than ${max}`);
+  }
+
+  return str;
+}
+
+function minOrThrow<T extends string, Min extends number>(
+  str: MinLength<T, Min>, min: Min
+): string {
+  if(str.length > min) {
+    throw new Error(`${str} is shorter than ${min}`);
+  }
+
+  return str;
+}
+
+
+function exactOrThrow<T extends string, Exact extends number>(
+  str: MinLength<T, Exact>, exact: Exact
+): string {
+  if(str.length !== exact) {
+    throw new Error(`${str} is not exact ${exact}`);
+  }
+
+  return str;
+}
+
+function inRangeOrThrow<T extends string, Min extends number, Max extends number>(
+  str: InRange<T, Min, Max>, min: Min, max: Max
+): string {
+  if(str.length > max) {
+    throw new Error(`${str} is longer than ${max}`);
+  } else if(str.length < min) {
+    throw new Error(`${str} is shorter than ${min}`);
+  }
+
+  return str;
+}
+
+
+
+const a = 'Test'
+
+inRangeOrThrow(a, 1, 4);
